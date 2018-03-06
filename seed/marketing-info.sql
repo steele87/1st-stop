ef26fc1668953926d0717b39334dc4f8e184ca97
James Steele <jamessteele87@outlook.com>
2018-03-06 15:12:05 +0000

    add tests on database

--- seed/marketing-info.sql
+++ seed/marketing-info.sql
@@ -1,10 +1,8 @@
 DROP DATABASE IF EXISTS marketing_info;
-
 CREATE DATABASE marketing_info;
-
 \c marketing_info;
-
-CREATE TABLE customer_info (
+CREATE TABLE customer_info
+(
   id SERIAL PRIMARY KEY,
   sms VARCHAR DEFAULT 'no',
   email VARCHAR DEFAULT 'no',
@@ -13,8 +11,17 @@ CREATE TABLE customer_info (
   datestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   customer_id INT
 );
-
-INSERT INTO customer_info (customer_id) VALUES
-(166445),(162703),(168027),(162084),(168310),(160155),(167223),(164459),(165888);
-
-SELECT * FROM customer_info;
+INSERT INTO customer_info
+  (customer_id)
+VALUES
+  (166445),
+  (162703),
+  (168027),
+  (162084),
+  (168310),
+  (160155),
+  (167223),
+  (164459),
+  (165888);
+SELECT *
+FROM customer_info;
