cc210c02d843d509651978339b95886fd9d69862
James Steele <jamessteele87@outlook.com>
2018-03-08 12:52:30 +0000

    add cors

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 const morgan = require('morgan');
 const apiRouter = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
 
