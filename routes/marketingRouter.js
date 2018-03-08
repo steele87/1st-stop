9e07e4960d8d3f8bf4a274c0907a8885302a1fba
James Steele <jamessteele87@outlook.com>
2018-03-08 12:27:31 +0000

    update route

--- routes/marketingRouter.js
+++ routes/marketingRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const updateMarketing = require('../controllers/marketing');
 
-router.route('/:user_id')
+router.route('/')
   .put(updateMarketing);
 
 module.exports = router;
